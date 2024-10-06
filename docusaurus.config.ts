ddc0608f6f59dce101652fc041946006e8b41a8f
Gary Ewan Park <gep13@gep13.co.uk>
2024-10-06 13:25:13 +0100

    Fix editUrl in docusaurus config (#1)
    After having a quick look at the new site, I saw that the links to edit
    both the docs and blog section were taking me over to a 404 in the
    GitHub repository. There was an extra "docs" and "blog" in the URLs that
    were being used.
    
    I actually have no idea whether this change will work, but based on what
    I am seeing being generated, it would appear to make sense.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/psake/docs/blob/main/docs',
+            'https://github.com/psake/docs/blob/main',
         },
         blog: {
           showReadingTime: true,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/psake/docs/blob/main/blog',
+            'https://github.com/psake/docs/blob/main',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
